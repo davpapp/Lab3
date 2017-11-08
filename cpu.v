2fa06519ce66145b86d4a34e78b2fbd53e678f25
Kimber <kimberly.winter@students.olin.edu>
2017-11-08 17:50:53 -0500

    Committing some top module changes

--- cpu.v
+++ cpu.v
@@ -1,7 +1,8 @@
 // Single cycle-cpu
 `include "ifetch.v"
 `include "control.v"
-`include "cpu.v"
+`include "datamemory.v"
+`include "register.v"
 
 // This is the top level module for our single cycle CPU
 // It consists of 5 sub-modules:
@@ -39,23 +40,31 @@ module cpu (
 	// This is the register I wrote for one of the HWs
 	wire[31:0] Da;
 	wire[31:0] Db;
-	reg regWrite = 1; // one of the controls - this should only be enabled for certain opcodes
 
 	wire writeData[31:0];
-	register Register(Da, Db, writeData, Rs, Rt, Rd, regWrite, clk); // Rd is incorrect here, will fix later
+	regfile register(Da, Db, writeData, Rs, Rt, Rd, regWrite, clk); // Rd is incorrect here, will fix later
+																	//Q: Could you include a testfile?
 
 // ----------------------------Execute-----------------------------------
+	always @(imm) //not sure if this is exactly correct, but not sure if this op can run continuously
+		extended_imm <= {{16{imm[15]}}, imm}; // extending the immediate
+	end
 	wire[31:0] extended_imm; // need to extend our immediate
-	wire[31:0] operandB;
-	mux ALUSource(ALU_OperandSource, Db, extended_imm); // choose between Db or our immediate as the second operand in the ALU
-
+	wire[31:0] Operand;	
+	mux ALUSource(Operand,ALU_OperandSource, Db, extended_imm); // choose between Db or our immediate as the second operand in the ALU
+							//Q: Db/ALU_operandsource???
 	// Use my ALU from Lab 1 - opcode will need to be converted
 	wire[31:0] ALU_result;
 	wire carryout, zero, overflow; // Don't think we actually use these
 	wire[2:0] command;
-	alu ALU(ALU_result, carryout, zero, overflow, Da, Db, command);
+	alu ALU(ALU_result, carryout, zero, overflow, Da, Operand, command[2:0]);
+
 // ----------------------------Memory/Write-----------------------------------
 //data memory, from lab 2:
 datamemory DM(clk,dataOut,address, WrEn,dataIn); 
-	
+mux ToReg(WriteData[31:0], memoryToRegister, ALU_result,dataOut); 
+
+//----------------------------Control-----------------------------------
+control CTL(opcode[5:0], regWrite, ALU_OperandSource,memoryRead,memoryWrite,memoryToRegister,command[2:0]); //inputs/outpus to control
+
 endmodule
